9e73aaae1b70a6d1150627250ed8095ba45e1a14
yoshihiro-6894 <54787477+yoshihiro-6894@users.noreply.github.com>
2022-02-09 21:16:12 +0900

    [wip]FadeManagerの追加
    シーン遷移でフェードインアウトするやつを作ります

---
+++ Assets/Scripts/common/FadeManager.cs
@@ -0,0 +1,61 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class FadeManager : MonoBehaviour
+{
+    public Image sprite;
+
+    public static FadeManager Instance = null;
+    private void Awake()
+    {
+        if (Instance != null)
+        {
+            Destroy(this.gameObject);
+            return;
+        }
+        Instance = this;
+        DontDestroyOnLoad(this.gameObject);
+    }
+
+    /// <summary>
+    /// 画面遷移 .
+    /// </summary>
+    /// <param name='scene'>シーン名</param>
+    /// <param name='interval'>暗転にかかる時間(秒)</param>
+    public void LoadScene(string scene, float interval)
+    {
+        StartCoroutine(TransScene(scene, interval));
+    }
+
+    /// <summary>
+    /// シーン遷移用コルーチン .
+    /// </summary>
+    /// <param name='scene'>シーン名</param>
+    /// <param name='interval'>暗転にかかる時間(秒)</param>
+    private IEnumerator TransScene(string scene, float interval)
+    {
+        //だんだん暗く .
+        float time = 0;
+        while (time <= interval)
+        {
+            sprite.color = new Color(0f, 0f, 0f, time / interval);
+            time += Time.deltaTime;
+            yield return null;
+        }
+
+        //シーン切替 .
+        SceneManager.LoadScene(scene);
+        
+        //だんだん明るく .
+        time = 0;
+        while (time <= interval)
+        {
+            sprite.color = new Color(0f, 0f, 0f, 1 - time / interval);
+            time += Time.deltaTime;
+            yield return null;
+        }
+    }
+}
