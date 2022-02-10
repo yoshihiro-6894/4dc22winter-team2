e51c27bdd6cc84d6bf557975d9a31332413b62ae
yoshihiro-6894 <54787477+yoshihiro-6894@users.noreply.github.com>
2022-02-10 16:53:30 +0900

    コスト(名誉)のチャージ等の実装

--- Assets/Scripts/CostManager.cs
+++ Assets/Scripts/CostManager.cs
@@ -1,26 +1,67 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class CostManager : MonoBehaviour
 {
-    public float MaxValue = 200;
+    public float MaxCostPoint = 200;
 
-    public float Value;
+    public float nowCostPoint;
+    private int UInowCostPoint;
 
-    public Text Uitext;
+    [Header("コストのチャージスピード")]public float ChargeSpeed;
+
+    public Text UItext;
 
     // Start is called before the first frame update
     void Start()
     {
-        Value = MaxValue;
+        nowCostPoint = 0;
+        UInowCostPoint = 0;
+        UItext.text = UInowCostPoint.ToString() + "/" + MaxCostPoint.ToString();
 
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        AddPoint(ChargeSpeed * Time.deltaTime);
+    }
+
+    public void AddPoint(float AddValue)
+    {
+        nowCostPoint += AddValue;
+
+        if (nowCostPoint >= MaxCostPoint)
+            nowCostPoint = MaxCostPoint;
+
+        UInowCostPoint = (int)nowCostPoint;
+
+        UItext.text = UInowCostPoint.ToString() + "/" + MaxCostPoint.ToString();
+    }
+
+    public void AddPoint(int AddValue)
+    {
+        nowCostPoint += AddValue;
+
+        if (nowCostPoint >= MaxCostPoint)
+            nowCostPoint = MaxCostPoint;
+
+        UInowCostPoint = (int)nowCostPoint;
+
+        UItext.text = UInowCostPoint.ToString() + "/" + MaxCostPoint.ToString();
+    }
+
+    public void DownPoint(int DownValue)
+    {
+        nowCostPoint -= DownValue;
+
+        if (nowCostPoint <= 0)
+            nowCostPoint = 0;
+
+        UInowCostPoint = (int)nowCostPoint;
+
+        UItext.text = UInowCostPoint.ToString() + "/" + MaxCostPoint.ToString();
     }
 }
