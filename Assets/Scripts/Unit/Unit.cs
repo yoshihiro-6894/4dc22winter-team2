a26e92f4b76f7f4c594e2aaa309d00d0e27caef6
yoshihiro-6894 <54787477+yoshihiro-6894@users.noreply.github.com>
2022-02-10 12:03:02 +0900

    Unitまわり始めます

---
+++ Assets/Scripts/Unit/Unit.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Unit : MonoBehaviour
+{
+    [Header("消えるまでの時間")]
+    public float DurationTime;
+
+    [Header("召喚コスト")]
+    public int Cost;
+
+    [Header("攻撃力")]
+    public int Atk;
+
+    [Header("攻撃間隔秒数")]
+    public float AttackInterval;
+
+
+}
