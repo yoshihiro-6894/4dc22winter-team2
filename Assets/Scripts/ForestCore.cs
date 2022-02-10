02456fcb887152d1b441afd90beb80c22c90f9e3
yoshihiro-6894 <54787477+yoshihiro-6894@users.noreply.github.com>
2022-02-10 14:35:34 +0900

    Unit2体目を用意しプロトタイプ作成
    コスト周りや重複防止などに努めます

--- Assets/Scripts/ForestCore.cs
+++ Assets/Scripts/ForestCore.cs
@@ -1,4 +1,4 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
@@ -14,12 +14,24 @@ public class ForestCore : MonoBehaviour
     private void Start()
     {
         Hp = MaxHp;
-        HpUItext.text = Hp.ToString();
+        HpUItext.text = Hp.ToString() + "/" + MaxHp.ToString();
     }
 
+    /// <summary>
+    /// 森のコアにダメージを与える関数
+    /// </summary>
+    /// <param name="damage">ダメージ量(正の自然数)</param>
     public void OnDamage(int damage)
     {
         Hp -= damage;
-        HpUItext.text = Hp.ToString();
+        HpUItext.text = Hp.ToString() + "/" + MaxHp.ToString();
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            OnDamage(5);
+        }
     }
 }
