c956b9db2c4d60152ffa8bc1bd1d20e7b313993e
CheshireMeCo <87901267+CheshireMeCo@users.noreply.github.com>
2022-02-11 21:58:06 +0900

    中継地点を追加したかった

--- Assets/Enemy1.cs
+++ Assets/Enemy1.cs
@@ -4,15 +4,20 @@ using UnityEngine;
 
 public class Enemy1 : MonoBehaviour
 {
+    public int hp;
+    public float speed;
+    public Route route;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+          
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        var v = Vector3.right;
+        transform.position += v * speed * Time.deltaTime;
     }
 }
