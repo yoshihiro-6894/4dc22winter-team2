abb8165307efbcacbb0b91c16f651d44a6c3b21e
yoshihiro-6894 <54787477+yoshihiro-6894@users.noreply.github.com>
2022-02-11 22:40:36 +0900

    修正

--- Assets/Enemy1.cs
+++ Assets/Enemy1.cs
@@ -6,7 +6,7 @@ public class Enemy1 : MonoBehaviour
 {
     public int hp;
     public float speed;
-    public Route route;
+    //public Route route;
 
     // Start is called before the first frame update
     void Start()
